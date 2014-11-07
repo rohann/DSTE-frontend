1d894b89529844f59371b3cc1bf01c04215c681e
rohann <rohannagrani@gmail.com>
2014-11-07 02:06:09 -0500

    final bugs

--- src/com/google/android/glass/awearable/ParseText.java
+++ src/com/google/android/glass/awearable/ParseText.java
@@ -121,7 +121,7 @@ public class ParseText {
 				}
 				else if(item!=""){
 					jsonArray = service.getProperty(item,"description");
-					response=parseProperty(jsonArray,property);
+					response=parseProperty(jsonArray,items);
 				}
 				else if(property!="")
 					response= "Which item do you need the "+property+" of?";
